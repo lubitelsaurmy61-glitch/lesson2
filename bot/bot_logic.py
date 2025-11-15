df7e7fc5d5b4f0ad8f9b595f5a703a0bd71308d1
lubitelsaurmy61-glitch <lubitelsaurmy61@gmail.com>
2025-11-15 10:29:38 +0300

    Update bot_logic.py

--- bot/bot_logic.py
+++ bot/bot_logic.py
@@ -1,4 +1,4 @@
-import random
+import random, requests
 
 def gen_pass(pass_length):
     elements = "+-/*!&$#?=@<>123456789"
@@ -10,3 +10,9 @@ def gen_pass(pass_length):
 def gen_emodji():
     emodji = ["\U0001f600", "\U0001f642", "\U0001F606", "\U0001F923"]
     return random.choice(emodji)
+
+def get_duck_image_url():    
+        url = 'https://random-d.uk/api/random'
+        res = requests.get(url)
+        data = res.json()
+        return data['url']
